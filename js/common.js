0a88100d36ecc216b5dde7028ebc0a3d77c61ee2
dabasjayant <jayantd1112@gmail.com>
2023-08-10 01:08:39 +0900

    navbar links

--- js/common.js
+++ js/common.js
@@ -14,21 +14,3 @@ $('#theme-icon').click(function (e) {
   $('body').addClass(theme);
   (theme === 'light') ? $('#theme-icon').html('&#xE708;') : $('#theme-icon').html('&#xE706;');
 });
-
-$('#nav-menu-btn').click(function (e) {
-  $('#overlay-backdrop').css('display', 'flex');
-  $('body').css('overflow', 'hidden');
-});
-
-$('#overlay-backdrop').click(function (e) {
-  $('#overlay-backdrop').css('display', 'none');
-  $('body').css('overflow', 'unset');
-});
-$('#nav-menu-close').click(function (e) { 
-  $('#overlay-backdrop').css('display', 'none');
-  $('body').css('overflow', 'unset');
-});
-
-$('#overlay-modal').click(function (e) {
-  e.stopPropagation();
-});
