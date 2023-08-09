25b3b687fc564abb74824e8410810cc9d5495c90
dabasjayant <jayantd1112@gmail.com>
2023-08-09 11:46:20 +0900

    add sections

--- js/common.js
+++ js/common.js
@@ -1,15 +1,17 @@
 $('#nav-menu-btn').click(function (e) {
   $('#overlay-backdrop').css('display', 'flex');
+  $('body').css('overflow', 'hidden');
 });
 
 $('#overlay-backdrop').click(function (e) {
   $('#overlay-backdrop').css('display', 'none');
+  $('body').css('overflow', 'unset');
 });
 $('#nav-menu-close').click(function (e) { 
-  $('#overlay-backdrop').css('display', 'none');  
+  $('#overlay-backdrop').css('display', 'none');
+  $('body').css('overflow', 'unset');
 });
 
-$('#overlay-modal').click(function (e) { 
-  e.preventDefault();
+$('#overlay-modal').click(function (e) {
   e.stopPropagation();
 });
